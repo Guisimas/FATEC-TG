2b0265d710d265b0d913ea51523b222797ba0727
Guisimas <guisimas45@gmail.com>
2025-07-03 14:54:48 -0300

    Att mistral

--- app/chat/mistral.py
+++ app/chat/mistral.py
@@ -2,4 +2,4 @@ from mistralai import Mistral
 
 
 def connect_mistral():
-    return Mistral(api_key="da6NnTH1RXeu77gxjvNjrgKLAxHqX4sQ")
+    return Mistral(api_key="F18jotQQnEbqzNXksaCglZKZAHckfQXg")
